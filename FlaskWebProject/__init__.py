d9e9d438dac88db8da7d204bc0ad0e9415b4d0cd
TZ <zhaotx@gmail.com>
2021-04-09 18:14:00 -0500

    Adding logging levels and handlers

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -6,11 +6,21 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from flask_session import Session
+from flask_session im  import session
+import os, sys
 
 app = Flask(__name__)
 app.config.from_object(Config)
 # TODO: Add any logging levels and handlers with app.logger
+# add logging levels
+logger = logging.getLogger('azure.storage')
+logger.setLevel(logging.INFO)
+# adding handler
+# Direct logging output to stdout. Without adding a handler,
+# no logging output is captured.
+handler = logging.StreamHandler(stream=sys.stdout)
+logger.addHandler(handler)
+
 Session(app)
 db = SQLAlchemy(app)
 login = LoginManager(app)
