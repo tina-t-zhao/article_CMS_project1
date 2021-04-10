9ac6c4eea1678227badd6d9841e7d2c7fad1eaab
TZ <zhaotx@gmail.com>
2021-04-10 17:36:52 -0500

    fix type im

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -6,7 +6,7 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from flask_session im  import session
+from flask_session import session
 import os, sys
 
 app = Flask(__name__)
