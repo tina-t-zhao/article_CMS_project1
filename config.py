70854bd1ce2774cff17240aad5a10767dc1f0e9f
TZ <zhaotx@gmail.com>
2021-04-10 18:08:39 -0500

    change server password

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ class Config(object):
     SQL_SERVER = os.environ.get('SQL_SERVER') or 'cms-article-servertx.database.windows.net'
     SQL_DATABASE = os.environ.get('SQL_DATABASE') or 'CMS-article-database'
     SQL_USER_NAME = os.environ.get('SQL_USER_NAME') or 'zhaotxtina'
-    SQL_PASSWORD = os.environ.get('SQL_PASSWORD') or 'Ran721ke933?1'
+    SQL_PASSWORD = os.environ.get('SQL_PASSWORD') or 'Azure12345'
     # Below URI may need some adjustments for driver version, based on your OS, if running locally
     SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://' + SQL_USER_NAME + '@' + SQL_SERVER + ':' + SQL_PASSWORD + '@' + SQL_SERVER + ':1433/' + SQL_DATABASE  + '?driver=ODBC+Driver+17+for+SQL+Server'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
