c9962a8f86697cca05782b365e4eaf566033ccca
TZ <zhaotx@gmail.com>
2021-04-14 14:41:32 -0500

    logging info successful add

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -68,16 +68,14 @@ def login():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
-
             app.logger.info('Log in attempt was UNSUCCESSFULL')
             return redirect(url_for('login'))
+        app.logger.info('Successful login')
         login_user(user, remember=form.remember_me.data)
-        app.logger.info()
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
 
             next_page = url_for('home')
-
         return redirect(next_page)
     session["state"] = str(uuid.uuid4())
     auth_url = _build_auth_url(scopes=Config.SCOPE, state=session["state"])
